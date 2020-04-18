18b15c4dcc338863cfd22201a9efd5d6f62dbc58
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-18 10:35:34 +0200

    Added configuration model

---
+++ az204api/az204api/Models/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace az204api.Models
+{
+    public class Configuration
+    {
+        public string KeyVaultUrl { get; set; }
+    }
+}
