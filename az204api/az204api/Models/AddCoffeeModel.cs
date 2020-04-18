31ed760c02b0441856ac8320a20645572388721d
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-18 10:41:32 +0200

    Added AddCoffeeModel

---
+++ az204api/az204api/Models/AddCoffeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace az204api.Models
+{
+    public class AddCoffeeModel
+    {        
+        public string Roastery { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public int Altitude { get; set; }
+        public string Process { get; set; }
+        public string BrewingMethod { get; set; }
+    }
+}
