673761663f6d924ebf337fc8907d98ebba44728c
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-18 14:21:03 +0200

    Static method to get CoffeeModel based on creation model

--- az204api/az204api/Models/CoffeeModel.cs
+++ az204api/az204api/Models/CoffeeModel.cs
@@ -15,5 +15,20 @@ namespace az204api.Models
         public int AltitudeMax { get; set; }
         public string Process { get; set; }
         public string BrewingMethod { get; set; }
+
+        public static CoffeeModel Create(AddCoffeeModel coffee)
+        {
+            return new CoffeeModel()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Roastery = coffee.Roastery,
+                Origin = coffee.Origin,
+                Region = coffee.Region,
+                AltitudeMax = coffee.AltitudeMax,
+                AltitudeMin = coffee.AltitudeMin,
+                Process = coffee.Process,
+                BrewingMethod = coffee.BrewingMethod
+            };
+        }
     }
 }
