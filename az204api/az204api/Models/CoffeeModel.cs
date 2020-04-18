c7d9182b08c1a2d6e1a03f7acfc14857c8191638
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-18 10:46:25 +0200

    Updated coffee model

--- az204api/az204api/Models/CoffeeModel.cs
+++ az204api/az204api/Models/CoffeeModel.cs
@@ -7,5 +7,12 @@ namespace az204api.Models
 {
     public class CoffeeModel
     {
+        public string Id { get; set; }
+        public string Roastery { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public int Altitude { get; set; }
+        public string Process { get; set; }
+        public string BrewingMethod { get; set; }
     }
 }
