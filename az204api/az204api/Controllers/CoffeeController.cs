d48061a48e5011145354dc04f5c39032b9cca33e
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-18 14:22:42 +0200

    Implemented addind document and querying based on roaster

--- az204api/az204api/Controllers/CoffeeController.cs
+++ az204api/az204api/Controllers/CoffeeController.cs
@@ -1,34 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using az204api.Models;
+﻿using az204api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace az204api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class CoffeeController : ControllerBase
-    {        
+    {
+        private const string DATABASE_ID = "Az204";
+        private const string CONTAINER_ID = "Coffees";
+        private readonly Container container;
         private readonly ILogger<CoffeeController> _logger;
 
-        public CoffeeController(ILogger<CoffeeController> logger)
+        public CoffeeController(CosmosClient client, ILogger<CoffeeController> logger)
         {
+            this.container = client.GetContainer(DATABASE_ID, CONTAINER_ID);
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<CoffeeModel> Get()
+        [HttpGet("{roastery}")]
+        public async Task<QueryResult> Get(string roastery, [FromQuery]string continuationToken = default(string))
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (!string.IsNullOrEmpty(continuationToken))
+                {
+                    var tokenBytes = Convert.FromBase64String(continuationToken);
+                    continuationToken = UTF8Encoding.UTF8.GetString(tokenBytes);
+                }
+                var query = $"select * from c where c.roastery = '{roastery}'";                
+                var iterator = container.GetItemQueryIterator<CoffeeModel>(query, continuationToken);
+                var result = await iterator.ReadNextAsync();                
+                return new QueryResult(result.ContinuationToken, result, result.RequestCharge);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get coffes from {roastery}", roastery);
+                throw;
+            }            
         }
 
         [HttpPost]
-        public IActionResult Add([FromBody]AddCoffeeModel coffee)
+        public async Task<IActionResult> Add([FromBody]AddCoffeeModel coffee)
         {
-            throw new NotImplementedException();
+            await container.CreateItemAsync(CoffeeModel.Create(coffee), new PartitionKey(coffee.Roastery));
+            return Ok();
         }
 
         [HttpDelete("{coffeeId}")]
