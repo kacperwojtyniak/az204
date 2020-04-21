249a64a737cba112822054b8b5cceb337d953342
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-21 19:58:42 +0200

    Added ability to select partition key to search

--- az204api/az204api/Controllers/CoffeeController.cs
+++ az204api/az204api/Controllers/CoffeeController.cs
@@ -31,7 +31,7 @@ namespace az204api.Controllers
             try
             {
                 var query = $"select * from c where c.roastery = '{roastery}'";
-                return await QueryAsync(query, continuationToken);
+                return await QueryAsync(query, continuationToken: continuationToken);
             }
             catch (Exception ex)
             {
@@ -40,12 +40,12 @@ namespace az204api.Controllers
             }            
         }
         [HttpGet("brewing/{brewingMethod}")]
-        public async Task<QueryResult> GetByBrewingMethod(string brewingMethod, [FromQuery]string continuationToken = default(string))
+        public async Task<QueryResult> GetByBrewingMethod(string brewingMethod, [FromQuery]string partitionKey, [FromQuery]string continuationToken = default(string))
         {
             try
             {
                 var query = $"select * from c where c.brewingMethod = '{brewingMethod}'";
-                return await QueryAsync(query, continuationToken);
+                return await QueryAsync(query, partitionKey, continuationToken);
             }
             catch (Exception ex)
             {
@@ -54,12 +54,12 @@ namespace az204api.Controllers
             }
         }
         [HttpGet("origin/{origin}")]
-        public async Task<QueryResult> GetByOrigin(string origin, [FromQuery]string continuationToken = default(string))
+        public async Task<QueryResult> GetByOrigin(string origin, [FromQuery]string partitionKey, [FromQuery]string continuationToken = default(string))
         {
             try
             {
                 var query = $"select * from c where c.origin = '{origin}'";
-                return await QueryAsync(query, continuationToken);
+                return await QueryAsync(query, partitionKey, continuationToken);
             }
             catch (Exception ex)
             {
@@ -74,7 +74,7 @@ namespace az204api.Controllers
             try
             {
                 var query = $"select * from c where c.id = '{id}'";                
-                return await QueryAsync(query, null);
+                return await QueryAsync(query);
             }
             catch (Exception ex)
             {
@@ -98,15 +98,17 @@ namespace az204api.Controllers
             return Ok();
         }
 
-        private async Task<QueryResult> QueryAsync(string query, string continuationToken)
+        private async Task<QueryResult> QueryAsync(string query, string partitionKey = null, string continuationToken = null)
         {
+            var queryOptions = string.IsNullOrEmpty(partitionKey) ? null : new QueryRequestOptions() { PartitionKey = new PartitionKey(partitionKey) };
             if (!string.IsNullOrEmpty(continuationToken))
             {
                 var tokenBytes = Convert.FromBase64String(continuationToken);
                 continuationToken = UTF8Encoding.UTF8.GetString(tokenBytes);
             }
 
-            var iterator = container.GetItemQueryIterator<CoffeeModel>(query, continuationToken);
+
+            var iterator = container.GetItemQueryIterator<CoffeeModel>(query, continuationToken, queryOptions);
             var result = await iterator.ReadNextAsync();
             return new QueryResult(result.ContinuationToken, result, result.RequestCharge);
         }
