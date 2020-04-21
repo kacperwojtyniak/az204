89bcf58efc0168f82225ecdf60ed04551ef723aa
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-21 18:04:13 +0200

    Added PS command to create container

---
+++ Scripts/manageAccount.ps1
@@ -0,0 +1,24 @@
+$resourceGroupName = "az204"
+$accountName = "az204db"
+$databaseName = "Az204"
+$databaseRUs = 400
+$containerName = "Coffees"
+$partitionKeyPath = "/roastery"
+
+Connect-AzAccount
+
+# Create DB
+Set-AzCosmosDBSqlDatabase `
+    -ResourceGroupName $resourceGroupName `
+    -AccountName $accountName `
+    -Name $databaseName `
+    -Throughput $databaseRUs
+
+#Create container
+    Set-AzCosmosDBSqlContainer `
+    -ResourceGroupName $resourceGroupName `
+    -AccountName $accountName `
+    -DatabaseName $databaseName `
+    -Name $containerName `
+    -PartitionKeyKind Hash `
+    -PartitionKeyPath $partitionKeyPath
