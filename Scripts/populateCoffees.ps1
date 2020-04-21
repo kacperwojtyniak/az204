78aa6571913a082b6bc523b70c6f0ed96f9eedaf
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-21 19:59:14 +0200

    Script to generate random coffees

--- Scripts/populateCoffees.ps1
+++ Scripts/populateCoffees.ps1
@@ -8,3 +8,23 @@ $coffeeJson = $coffee | ConvertTo-Json
 Invoke-WebRequest -Uri $url -Method POST -Body $coffeeJson -ContentType "application/json"
 }
 
+$origins = "Ethiopia", "Peru", "Guatemala", "Brazil", "Salvador", "Columbia","Nicaragua","Burundi"
+
+ForEach($origin in $origins)
+{
+    For ($i=0; $i -le 500; $i++) {
+        $coffee = @{
+            name = "test$($i)";
+            roastery = "Roastery$($i)";
+            origin = $origin;
+            region = "$($origin)Region$($i)";
+            altitudeMin = Get-Random -Minimum 1000 -Maximum 1200;
+            altitudeMax = Get-Random -Minimum 1201 -Maximum 1850;
+            process = "Washed", "Natural" | Get-Random;
+            brewingMethod = "filter", "espresso", "all" | Get-Random;
+        }
+         $json = $coffee | ConvertTo-Json
+         Invoke-WebRequest -Uri $url -Method POST -Body $json -ContentType "application/json"
+        }
+}
+
