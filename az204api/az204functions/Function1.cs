fb27bf8f071139dc1ac1e6e02000a76e1adf8bba
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-25 09:47:03 +0200

    Second function to copy documents to separate collection
    Making use of two separate leases collections

--- az204api/az204functions/Function1.cs
+++ az204api/az204functions/Function1.cs
@@ -9,26 +9,49 @@ using Microsoft.Extensions.Logging;
 
 namespace az204functions
 {
-    public static class Function1
+    public class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task Run([CosmosDBTrigger(
+        [FunctionName("CopyToCoffeesBrewing")]
+        public async Task CopyToCoffeesBrewing([CosmosDBTrigger(
             databaseName: "%databaseName%",
             collectionName: "%inputCollection%",
             ConnectionStringSetting = "connectionString",
-            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input,
+            CreateLeaseCollectionIfNotExists = true,
+            LeaseCollectionName ="leaseBrewing")]IReadOnlyList<Document> input,
             [CosmosDB(
                 databaseName: "%databaseName%",
-                collectionName: "%outputCollection%",
+                collectionName: "%outputCollectionBrewing%",
                 ConnectionStringSetting = "connectionString")]
                 DocumentClient client, ILogger log)
         {
+            var outputCollection = Environment.GetEnvironmentVariable("outputCollectionBrewing");
+            await Copy(input, client, outputCollection);
+        }
+
+        [FunctionName("CopyToCoffeesOrigin")]
+        public async Task CopyToCoffeesOrigin([CosmosDBTrigger(
+            databaseName: "%databaseName%",
+            collectionName: "%inputCollection%",
+            ConnectionStringSetting = "connectionString",
+            CreateLeaseCollectionIfNotExists = true,
+            LeaseCollectionName ="leaseOrigin")]IReadOnlyList<Document> input,
+            [CosmosDB(
+                databaseName: "%databaseName%",
+                collectionName: "%outputCollectionOrigin%",
+                ConnectionStringSetting = "connectionString")]
+                DocumentClient client, ILogger log)
+        {
+            var outputCollection = Environment.GetEnvironmentVariable("outputCollectionOrigin");
+            await Copy(input, client, outputCollection);
+        }
+
+        private async Task Copy(IReadOnlyList<Document> input, DocumentClient client, string outputCollection)
+        {
             var databaseName = Environment.GetEnvironmentVariable("databaseName");
-            var outputCollection = Environment.GetEnvironmentVariable("outputCollection");
             var collectionUri = UriFactory.CreateDocumentCollectionUri(databaseName, outputCollection);
             foreach (var coffee in input)
-            {                
-                await client.UpsertDocumentAsync(collectionUri, coffee);                
+            {
+                await client.UpsertDocumentAsync(collectionUri, coffee);
             }
         }
     }
