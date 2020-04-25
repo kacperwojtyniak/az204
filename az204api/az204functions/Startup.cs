0aff3031128c81eefef523467b4dc65f35fbe89b
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-25 15:26:28 +0200

    Added startup class

---
+++ az204api/az204functions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(az204functions.Startup))]
+namespace az204functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient("orders-logicapp",x=> x.BaseAddress = new Uri(Environment.GetEnvironmentVariable("ordersLogicappUrl")));
+        }
+    }
+}
