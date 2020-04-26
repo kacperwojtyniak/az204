95938bbb27327ecd434f13f9b254e407e5cee822
Kacper Wojtyniak <kacper.wojtyniak@krollontrack.com>
2020-04-26 10:23:58 +0200

    Empty function to request order approval

--- az204api/az204functions/OrdersFunctions.cs
+++ az204api/az204functions/OrdersFunctions.cs
@@ -1,36 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using az204functions.Models;
-using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Client;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace az204functions
 {
-    public class OrdersFunctions
-    {
-        private HttpClient client;
+	public class OrdersFunctions
+	{
+		private HttpClient client;
 
-        public OrdersFunctions(IHttpClientFactory httpClientFactory)
-        {
-            this.client = httpClientFactory.CreateClient("orders-logicapp");
-        }
-        [FunctionName("ValidateOrder")]
-        public async Task ValidateOrder([CosmosDBTrigger(
-            databaseName: "%databaseName%",
-            collectionName: "%ordersCollection%",
-            ConnectionStringSetting = "connectionString",
-            CreateLeaseCollectionIfNotExists = true,
-            LeaseCollectionName ="leaseOrders")]IReadOnlyList<Document> input, ILogger log)
-        {
-            throw new NotImplementedException();
-        }
-       
-    }
+		public OrdersFunctions(IHttpClientFactory httpClientFactory)
+		{
+			this.client = httpClientFactory.CreateClient("orders-logicapp");
+		}
+		[FunctionName("ValidateOrder")]
+		public async Task ValidateOrder([CosmosDBTrigger(
+			databaseName: "%databaseName%",
+			collectionName: "%ordersCollection%",
+			ConnectionStringSetting = "connectionString",
+			CreateLeaseCollectionIfNotExists = true,
+			LeaseCollectionName ="leaseOrders")]IReadOnlyList<Document> input, ILogger log)
+		{
+			throw new NotImplementedException();
+		}
+
+		[FunctionName("RequestApproval")]
+		public async Task RequestApproval([HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
 }
