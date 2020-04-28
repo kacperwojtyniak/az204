c02d328c627fdbea921a375f93d4b135337355ae
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-28 20:00:59 +0200

    Removed unused property

--- az204api/az204functions/Models/OrderModel.cs
+++ az204api/az204functions/Models/OrderModel.cs
@@ -8,6 +8,5 @@ namespace az204functions.Models
         public string Date { get; set; }
         public decimal TotalAmount { get; set; }
         public OrderStatus Status { get; set; }
-        public IEnumerable<OrderItem> Items { get; set; }
     }
 }
