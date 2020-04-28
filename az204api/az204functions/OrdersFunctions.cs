83916cfb1135338e80a75f53faba3fb159e46a43
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-04-28 17:19:26 +0200

    Passing order object in functions

--- az204api/az204functions/OrdersFunctions.cs
+++ az204api/az204functions/OrdersFunctions.cs
@@ -37,16 +37,17 @@ namespace az204functions
 		[FunctionName("RequestApproval")]
 		public async Task RequestApproval([HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req, [DurableClient] IDurableClient starter)
 		{
-			var orderId = req.Query["id"];
-			var res = await starter.StartNewAsync<string>("WaitForOrderApproval", input: orderId);
+			var orderId = req.Query["id"][0];
+			var date = req.Query["date"][0];
+			await starter.StartNewAsync<OrderModel>("WaitForOrderApproval", input: new OrderModel (){ Id = orderId, Date = date });
 
 		}
 
 		[FunctionName("WaitForOrderApproval")]
 		public async Task WaitForOrderApproval([OrchestrationTrigger]IDurableOrchestrationContext context)
 		{
-			var orderId = context.GetInput<string>();
-			await context.CallActivityAsync("SendApprovalRequest", orderId); // Send email/sms whatever. Right now just add to table
+			var input = context.GetInput<OrderModel>();
+			await context.CallActivityAsync<OrderModel>("SendApprovalRequest", input); // Send email/sms whatever. Right now just add to table
 
 			using var timeoutCts = new CancellationTokenSource();
 			DateTime dueTime = context.CurrentUtcDateTime.AddMinutes(5);
@@ -59,11 +60,13 @@ namespace az204functions
 				timeoutCts.Cancel();
 				if (approvalEvent.Result.Approved)
 				{
-					await context.CallActivityAsync("ApproveOrder", orderId);
+					input.Status = "Approved";
+					await context.CallActivityAsync<OrderModel>("SetStatus", input);
 					return;
 				};
 			}
-			await context.CallActivityAsync("RejectOrder", orderId);
+			input.Status = "Rejected";
+			await context.CallActivityAsync<OrderModel>("SetStatus", input);
 
 		}
 		
@@ -74,23 +77,23 @@ namespace az204functions
 				ConnectionStringSetting = "connectionString")]out dynamic document)
 		{
 			// Should send email/sms/some notification. Now just adding a document to collecion
-			var orderId = helloContext.GetInput<string>();
+			var orderId = helloContext.GetInput<OrderModel>().Id;
 			var hostname = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
-			var url = $"https://{hostname}/runtime/webhooks/durabletask/instances/{helloContext.InstanceId}/raiseEvent/ApprovalEvent";
+			var url = $"http://{hostname}/runtime/webhooks/durabletask/instances/{helloContext.InstanceId}/raiseEvent/ApprovalEvent";
 			document = new { date = DateTime.UtcNow.ToShortDateString(), url = url, type = "Approval", status = "pending", orderId = orderId };
 		}
 
-		[FunctionName("ApproveOrder")]
-		public void ApproveOrder([ActivityTrigger] string instanceId)
+		[FunctionName("SetStatus")]
+		public async Task SetStatus([ActivityTrigger]IDurableActivityContext context, [CosmosDB(
+				databaseName: "%databaseName%",
+				collectionName: "%ordersCollection%",
+				ConnectionStringSetting = "connectionString")] DocumentClient client)
 		{
-
+			var input = context.GetInput<OrderModel>();
+			var databaseName = Environment.GetEnvironmentVariable("databaseName");
+			var collection = Environment.GetEnvironmentVariable("ordersCollection");
+			var procedure = UriFactory.CreateStoredProcedureUri(databaseName, collection, "setOrderStatus");
+			await client.ExecuteStoredProcedureAsync<dynamic>(procedure,new RequestOptions() { PartitionKey = new PartitionKey(input.Date) }, input.Id, "Approved");
 		}
-
-		[FunctionName("RejectOrder")]
-		public void RejectOrder([ActivityTrigger] string instanceId)
-		{
-
-		}
-
 	}
 }
