5eae69ba745d4eca5fe28006fcaf17e90b124dd8
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-05-07 19:23:37 +0200

    Implemented adding coffee picture

--- az204api/az204api/Controllers/PicturesController.cs
+++ az204api/az204api/Controllers/PicturesController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using az204api.Models;
 using Azure.Identity;
 using Azure.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using static az204api.Constants;
 
 namespace az204api.Controllers
 {
@@ -15,18 +20,37 @@ namespace az204api.Controllers
     [ApiController]
     public class PicturesController : ControllerBase
     {
-        [HttpPost("{id}")]
-        public async Task<IActionResult> Post(string id, [FromForm]IFormFile file)
+        private readonly BlobServiceClient blobClient;
+        private Container container;
+        private const string COFFEES_BLOB_CONTAINER = "coffees";
+        public PicturesController(CosmosClient cosmosClient, BlobServiceClient blobClient)
+        {
+            this.container = cosmosClient.GetContainer(DATABASE_ID, COFFEES_CONTAINER);
+            this.blobClient = blobClient;
+        }
+
+        [HttpPost("{id}/{roastery}")]
+        public async Task<IActionResult> Post(string id, string roastery, [FromForm]IFormFile file)
         {
             try
             {
-                var containerEndpoint = "https://az204coffee.blob.core.windows.net/";
-                var client = new BlobServiceClient(new Uri(containerEndpoint), new DefaultAzureCredential());                
+                if (file.ContentType != "image/jpeg")
+                {
+                    return new UnsupportedMediaTypeResult();
+                }
+                var coffee = await container.ReadItemAsync<CoffeeModel>(id, new PartitionKey(roastery));
+                var fileName = $"{coffee.Resource.Name.Replace(" ", "_").Trim()}.jpg";
 
-                var containerClient = client.GetBlobContainerClient("coffees");
-                using var stream = file.OpenReadStream();
-                var response = await containerClient.UploadBlobAsync(file.FileName, stream);                
-                return Ok();
+                var containerClient = blobClient.GetBlobContainerClient(COFFEES_BLOB_CONTAINER).GetBlobClient(fileName);
+
+                using var stream = file.OpenReadStream();                
+                await containerClient.UploadAsync(stream, new BlobHttpHeaders() { ContentType = file.ContentType });
+                
+                var blobUri = $"{blobClient.Uri}{COFFEES_BLOB_CONTAINER}/{fileName}";
+                coffee.Resource.ImgUrl = blobUri;
+                await container.UpsertItemAsync(coffee.Resource);
+
+                return Ok(coffee.Resource);
             }
             catch (Exception ex)
             {
