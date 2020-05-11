44ce48ed1936512416049a251eabd65db6d8625b
Kacper Wojtyniak <kacper.wojtyniak@krollontrack.com>
2020-05-11 16:16:45 +0200

    Working on Arm

--- Scripts/ArmTemplates/deployTemplate.ps1
+++ Scripts/ArmTemplates/deployTemplate.ps1
@@ -1,14 +1,14 @@
 $resourceGroupName = "TestTemplates"
 $templatePath = "./template.json"
+$templateParameterPath = "./template.parameters.json"
 $location = "EastUs"
 
 #Connect-AzAccount
 
 $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 
-if (!$resourceGroup)
-{    
-    New-AzResourceGroup -Name $resourceGroupName -Location $location
+if (!$resourceGroup) {    
+  New-AzResourceGroup -Name $resourceGroupName -Location $location
 }
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatePath
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatePath -TemplateParameterFile $templateParameterPath
