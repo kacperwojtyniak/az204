72c7e73ec03fec7f3424b8c5f1617924adc2348d
Kacper Wojtyniak <wojtyniak.kacper@gmail.com>
2020-05-21 21:06:15 +0200

    Splitting things up, deploying container instances

--- Scripts/ArmTemplates/deployTemplate.ps1
+++ Scripts/ArmTemplates/deployTemplate.ps1
@@ -1,4 +1,6 @@
 $resourceGroupName = "TestTemplates"
+$acrResourceGroupName = "AcrResourceGroup"
+$acrTemplatePath = "./acrTemplate.json"
 $templatePath = "./template.json"
 $logicApptemplatePath = "./logicApp.json"
 $logicAppParameterPath = "./logicApp.parameters.json"
@@ -7,20 +9,41 @@ $location = "EastUs"
 $functionsPath = "../../az204api/az204functions"
 $publishPath = $functionsPath + '/bin/debug/netcoreapp3.1/*'
 
-az login
+Connect-AzAccount
 
-$resourceGroup = az group exists --name $resourceGroupName
-
-if (!$resourceGroup) {    
-  az group create --name $resourceGroupName --location $location
+if (!(Get-AzResourceGroup -Name $resourceGroupName)) {    
+  New-AzResourceGroup -Name $resourceGroupName -Location $location
 }
 
-az group deployment create --resource-group $resourceGroupName --template-file $templatePath --parameters $templateParameterPath
+if(!(Get-AzResourceGroup -Name $acrResourceGroupName)) {
+  New-AzResourceGroup -Name $acrResourceGroupName -Location $location
+}
 
+#Deploy acr
+$registryName = (New-AzResourceGroupDeployment -ResourceGroupName $acrResourceGroupName -TemplateFile $acrTemplatePath).Outputs.acrName.Value
+
+#Push image to registry
+$registry = Get-AzContainerRegistry -ResourceGroupName $acrResourceGroupName -Name $registryName
+$creds = Get-AzContainerRegistryCredential -Registry $registry
+$creds.Password | docker login $registry.LoginServer -u $creds.Username --password-stdin
+$imageName = "$($registry.LoginServer)/az204api:latest"
+docker build -f ..\..\az204api\az204api\Dockerfile -t $imageName ..\..\az204api
+docker push $imageName
+
+
+
+#Deploy main resource group
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatePath -TemplateParameterFile $templateParameterPath `
+-acrUserName $creds.Username `
+-acrPassword $creds.Password `
+-imageName $imageName `
+-acrServerName $registry.LoginServer
+
+#Build azure functions, zip and deploy
 dotnet publish $functionsPath
 Compress-Archive -Path $publishPath -DestinationPath ./function.zip
 
 az functionapp deployment source config-zip -g $resourceGroupName -n 'az204functions123' --src './function.zip'
 
-az group deployment create --resource-group $resourceGroupName --template-file $logicApptemplatePath --parameters $logicAppParameterPath
-
+#Deploy logic apps
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $logicApptemplatePath -TemplateParameterFile $logicAppParameterPath
